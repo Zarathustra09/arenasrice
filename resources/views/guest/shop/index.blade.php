ef6ab52b50561f452219f2ac782135cf62654ef1
Zarathustra09 <joshua.pardo30@gmail.com>
2025-02-27 19:26:13 +0800

    February 27 2025 - removed kg inside index.blade.php

--- resources/views/guest/shop/index.blade.php
+++ resources/views/guest/shop/index.blade.php
@@ -35,7 +35,7 @@
                                             <div class="card-body">
                                                 <h5 class="card-title">{{ $product->name }}</h5>
                                                 <p class="card-text">{{ $product->description }}</p>
-                                                <p class="card-text">₱{{ $product->price }}/kg</p>
+                                                <p class="card-text">₱{{ $product->price }}</p>
                                                 <form class="add-to-cart-form" action="{{ route('cart.store') }}" method="POST">
                                                     @csrf
                                                     <input type="hidden" name="product_id" value="{{ $product->id }}">
